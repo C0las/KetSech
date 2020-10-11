3112824a9585c5d6e1c2baa917a2a8c2da5caf7e
Nicolas Henriquez <nhenriquez619@gmail.com>
2020-10-10 23:09:45 -0300

    Updates

---
+++ precache-manifest.9eb37f51136946a9ed3189ac4efc4434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cbd5d530344aeb57253490d31f0197",
+    "url": "/KetSech/index.html"
+  },
+  {
+    "revision": "5b4b3942f228ed01a3fd",
+    "url": "/KetSech/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "796d6c3ca2560b80805c",
+    "url": "/KetSech/static/js/2.139719c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KetSech/static/js/2.139719c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4b3942f228ed01a3fd",
+    "url": "/KetSech/static/js/main.bd6ab85c.chunk.js"
+  },
+  {
+    "revision": "d2267f925ccae84595c0",
+    "url": "/KetSech/static/js/runtime-main.572ae08d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/KetSech/static/media/logo.ee7cd8ed.svg"
+  }
+]);
